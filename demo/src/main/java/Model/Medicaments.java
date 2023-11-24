944391fd176b2f86dfa292762f50800842bd5249
RaulMagaña <maganaraul349@hotmail.com>
2023-11-24 11:52:41 -0600

    Medicament Class

---
+++ demo/src/main/java/Model/Medicaments.java
@@ -0,0 +1,30 @@
+package demo.src.main.java.Model;
+
+public class Medicaments {
+    private int id;
+    private String name;
+
+    public Medicaments(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return name;
+    }
+
+    public void setNombre(String name) {
+        this.name = name;
+    }
+
+
+    
+}
