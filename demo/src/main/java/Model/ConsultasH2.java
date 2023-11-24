b782b78aacc89e3c2ddfb8f6ff9ec74b711154b2
RaulMagaña <maganaraul349@hotmail.com>
2023-11-24 12:07:34 -0600

    Intento de conexion

--- demo/src/main/java/Model/ConsultasH2.java
+++ demo/src/main/java/Model/ConsultasH2.java
@@ -1,5 +1,29 @@
 package demo.src.main.java.Model;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class ConsultasH2 {
     
+    public void insertarMedicamento(Medicaments medicamento) {
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO medicamentos (nombre, descripcion) VALUES (?, ?)")) {
+
+            preparedStatement.setString(1, medicamento.getNombre());
+            preparedStatement.setString(2, medicamento.getDescripcion());
+
+            preparedStatement.executeUpdate();
+
+            System.out.println("Medicamento insertado correctamente.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        private Connection getConnection() {
+            Conexion conexion = new Conexion();
+            return conexion.getConexion();
+        }
+        
+    }
 }
--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -1,7 +1,12 @@
 package demo.src.main.java.com.example;
+import demo.src.main.java.Controller.H2DBController;
+import demo.src.main.java.Model.ConsultasH2;
+import demo.src.main.java.Model.Medicaments;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ConsultasH2 consultasH2 = new ConsultasH2();
+        consultasH2.insertarMedicamento(new Medicaments(1, "Medicamento Ejemplo", "Descripción del medicamento"));
     }
 }
